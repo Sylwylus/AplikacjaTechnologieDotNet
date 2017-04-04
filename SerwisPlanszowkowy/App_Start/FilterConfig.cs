92f97c1545e8efaba8d5ae135268f8175674b630
sylka <sylwia.jakubas@gmail.com>
2017-04-04 17:45:10 +0200

    Szkielet aplikacji

---
+++ SerwisPlanszowkowy/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SerwisPlanszowkowy
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
